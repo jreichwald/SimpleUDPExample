074e338ca32e839ac30193d0d5e18b82eeb11d7e
Prof. Dr. Julian Reichwald <julian.reichwald@dhbw-mannheim.de>
2015-02-18 22:32:29 +0100

    Created Server Code

--- src/main/java/jreichwald/dhbw/vs/assignment1/SimpleUDPServer.java
+++ src/main/java/jreichwald/dhbw/vs/assignment1/SimpleUDPServer.java
@@ -1,5 +1,7 @@
 package jreichwald.dhbw.vs.assignment1;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,10 +17,33 @@ public class SimpleUDPServer {
 	private static Logger _log = LogManager.getLogger(SimpleUDPServer.class);
 	
 	public static void main(String[] args) {
-		_log.debug("Server starting.");
 		
+		DatagramSocket serverSocket = null; 
 		
-		
-	}
+		try {
+			//Create server socket on port 11111
+			serverSocket = new DatagramSocket(11111);
+			
+			//Create Message Buffer 
+			byte[] buffer = new byte[10];
+			
+			while (true) {
+				_log.debug("Server waiting for UDP datagrams ...");
+				
+				//Create a datagram packet using the message buffer
+				DatagramPacket dp_request = new DatagramPacket(buffer, buffer.length);
 
+				//wait for incoming datagram: blocking call
+				serverSocket.receive(dp_request);
+				
+				//extract message from datagram packet
+				String str = new String(dp_request.getData());
+				
+				_log.debug("Message received. Content: " + str);
+			}
+			
+		} catch (Exception e) {
+			_log.error(e);
+		}	
+	}
 }
