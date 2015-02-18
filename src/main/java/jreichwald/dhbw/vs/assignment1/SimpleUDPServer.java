bdb1c490a3d962060ad9a0915ddedf1a0c769e15
Prof. Dr. Julian Reichwald <julian.reichwald@dhbw-mannheim.de>
2015-02-18 22:22:10 +0100

    Initial Commit with first code class

---
+++ src/main/java/jreichwald/dhbw/vs/assignment1/SimpleUDPServer.java
@@ -0,0 +1,24 @@
+package jreichwald.dhbw.vs.assignment1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * Implementation of a simple UDP server which accepts a message 
+ * and prints the results to a console log
+ * @author Prof. Dr. Julian Reichwald <julian.reichwald@dhbw-mannheim.de>
+ *
+ */
+public class SimpleUDPServer {
+
+	private static Logger _log = LogManager.getLogger(SimpleUDPServer.class);
+	
+	public static void main(String[] args) {
+		_log.debug("Server starting.");
+		
+		
+		
+	}
+
+}
